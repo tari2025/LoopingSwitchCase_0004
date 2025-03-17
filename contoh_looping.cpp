77f8755f2f9f4de07b6d2da223e55b613774224a
tari2025 <denilestari272@gmail.com>
2025-03-17 11:25:04 +0700

    perulangan dengan for

--- contoh_looping.cpp
+++ contoh_looping.cpp
@@ -15,13 +15,37 @@ int main()
         cout << "Nilai n = " << "Selamat datang" << endl;
     }
     cout << "Nilai n terakhir = " << n << endl;
-cout << endl'
+    cout << endl;
 
-cout << perulangan for pencacah turun" << endl;
+    cout << "perulangan for pencacah turun" << endl;
 
-// perulangan dengan for
-for (n = 5; n > 0; n--)
-{
-    cout << nilai n = " << n << "Selamat datang" << endl;
+    // perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << nilai n = " << n << " Selamat datang " << endl;
+    }
+    cout << "Nilai n terakhir = " << n;
+    cout << endl;
+    cout << "Perulangan while" << endl;
+    srand(time(0));
+    n = rand() % 10;
+    cout << "Nilai awal n = " << endl;
+    while (n < 7)
+    {
+        cout << "Nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Jumlah perulangan = " << PerulanganWhile << endl;
+    cout << "Nilai n terakhir = " << endl;
+    cout << "Perulangan do while" << endl;
+    srand(time0));
+    n = rand() % 10;
+    cout << "Nilai awal n = " << n << endl;
+    n = rand() % 10;
+    perulanganDo++;
 }
+While(n < 7);
+cout << "Jumlah perulangan = " << perulanganWhile << endl;
+cout << "Nilai n terakhir = " << endl "
 }
