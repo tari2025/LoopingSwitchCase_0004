10fd8e26b46ade46ad6682e65beaf1376338ee78
tari2025 <denilestari272@gmail.com>
2025-03-17 11:45:56 +0700

    luas lingkaran

---
+++ switchCase.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+float luasPersegiPanjang(float a, floatb)
+{
+    return a * b;
+}
+
+int main()
+{
+    int pilihan;
+    float panjang, lebar;
+    float jejari;
+
+    do
+    {
+        cout << "Menu" << endl;
+
+        float luasLingkaran(float a) return 3.14 * a * a;
+        {
+        }
+        int main()
+        {
+            int pilihan;
+            float panjang, lebar;
+            float jejari;
+
+            do
+            {
+                cout << "Menu" << endl;
+                cout << "1. Luas persegi Panjang" << endl;
+                cout << "2. Luas Lingkaran" << endl;
+                cout << "3. Keluar" << endl;
+
+                cout << "Pilihan (1/2/3) = ";
+                cin >> piliham;
+            swith (pilihan)
+
+            case 1
+        }
+}
+
+    }
+}
+}
