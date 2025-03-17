6722c4511324bf5bd227a43c69f48dcc14248e8f
tari2025 <denilestari272@gmail.com>
2025-03-17 10:54:51 +0700

    menambahkan fungsi

--- contoh_looping.cpp
+++ contoh_looping.cpp
@@ -1,9 +1,21 @@
 #include
 using namespace std;
 
-int main ()
+int main()
 {
     int n;
     int perulanganWhile = 0;
-    int perulanganDo;
+    int perulanganDo = 0;
+
+    // Perulangan dengan for
+
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << "Selamat datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+cout << endl'
+
+cout << perulangan for pencacah turun
 }
